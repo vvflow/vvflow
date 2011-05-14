869688a436d397381759cdabf49b9e06da9114b2
Yaroslav Dynnikov <rosik.ru@gmail.com>
2011-05-14 20:29:41 +0400

    update DiffMergeFast
    Former-commit-id: 7fd88a366878de1ac331819b85a1bfd95d7579bf

--- source/modules/diffmergefast.cpp
+++ source/modules/diffmergefast.cpp
@@ -24,7 +24,7 @@ double GRestriction;
 double DiffMergeFast_MergeSqEps;
 int DiffMergeFast_MergedV;
 
-void MergeVortexes(TObject **lv1, TObject **lv2);
+void MergeVortexes(TObj **lv1, TObj **lv2);
 
 enum ParticleType {Vortex, Heat};
 }
@@ -37,18 +37,18 @@ int InitDiffMergeFast(Space *sS, double sRe, double sMergeSqEps)
 	DiffMergeFast_Re = sRe;
 	DiffMergeFast_Nyu = 1/sRe;
 	DiffMergeFast_MergeSqEps = sMergeSqEps;
-	EpsRestriction = (sS->Body) ? 0.6*sS->Body->SurfaceLength()/sS->Body->List->size : 0;
+	EpsRestriction = (sS->Body) ? 0.6*sS->Body->AverageSegmentLength() : 0;
 	//DiffMergeFast_dfi = (sS->BodyList) ? C_2PI/sS->BodyList->size : 0;
 	return 0;
 }
 
 namespace {
-void MergeVortexes(TObject **lv1, TObject **lv2)
+void MergeVortexes(TObj **lv1, TObj **lv2)
 {
 	if (!lv1 || !lv2 || (lv1==lv2)) return;
 	if (!*lv1 || !*lv2) return;
-	TObject &v1 = **lv1;
-	TObject &v2 = **lv2;
+	TObj &v1 = **lv1;
+	TObj &v2 = **lv2;
 	//if (fabs(v1.g + v2.g) > GRestriction) return -1;
 	DiffMergeFast_MergedV++;
 
@@ -56,7 +56,7 @@ void MergeVortexes(TObject **lv1, TObject **lv2)
 	{
 		v1 = (v1*v1.g + v2*v2.g)/(v1.g + v2.g);
 	}
-	else if ( fabs(v1.g) < fabs(v2.g) ) { Vector(v1) = Vector(v2); }
+	else if ( fabs(v1.g) < fabs(v2.g) ) { TVec(v1) = TVec(v2); }
 	v1.v = (v1.v*v1.g + v2.v*v2.g)/(v1.g + v2.g);
 	v1.g+= v2.g;
 	v2.g = 0;
@@ -70,49 +70,38 @@ int DiffMergedFastV()
 
 namespace {
 template <ParticleType pt>
-double Epsilon(const TNode &Node, TObject **lv, bool merge)
+double Epsilon(const TNode &Node, TObj **lv, bool merge)
 {
 	if (!lv || !*lv) { return 1E-20; }
-	Vector dr;
+	TVec dr;
 	double res1, res2;
 	res2 = res1 = 1E10;
 
-	TObject &v = **lv;
-	TObject **lv1, **lv2;
+	TObj &v = **lv;
+	TObj **lv1, **lv2;
 	lv1 = lv2 = NULL;
 
-	TNode **lNNode = Node.NearNodes->First;
-	TNode **&LastNNode = Node.NearNodes->Last;
-	for ( ; lNNode<LastNNode; lNNode++ )
+	auto nnodes = Node.NearNodes;
+	auto lNNode = nnodes->begin();
+	auto lNNodeEnd = nnodes->end();
+	for (; lNNode<lNNodeEnd; lNNode++ )
 	{
 		TNode &NNode = **lNNode;
 
-		TObject **lObj;
-		TObject ***lLastObj;
-		switch (pt)
-		{
-			case Vortex:
-				if ( !NNode.VortexLList ) { continue; }
-				lObj = NNode.VortexLList->First;
-				lLastObj = &NNode.VortexLList->Last;
-				break;
-			case Heat:
-				if ( !NNode.HeatLList ) { continue; }
-				lObj = NNode.HeatLList->First;
-				lLastObj = &NNode.HeatLList->Last;
-				break;
-		}
-		TObject **&LastObj = *lLastObj;
-
-		for ( ; lObj<LastObj; lObj++ )
+		vector<TObj*> *list;
+		list = (pt==Vortex) ? NNode.VortexLList : NNode.HeatLList;
+		if (!list) {continue;}
+		auto lObj = list->begin();
+		auto lObjEnd = list->end();
+		for (; lObj<lObjEnd; lObj++ )
 		{
 			if (!*lObj) { continue; }
-			TObject &Obj = **lObj;
+			TObj &Obj = **lObj;
 			dr = v - Obj;
 			double drabs2 = dr.abs2();
 			if (!drabs2) continue;
-			if ( res1 > drabs2 ) { res2 = res1; lv2 = lv1; res1 = drabs2; lv1 = lObj;} 
-			else if ( res2 > drabs2 ) { res2 = drabs2; lv2 = lObj; }
+			if ( res1 > drabs2 ) { res2 = res1; lv2 = lv1; res1 = drabs2; lv1 = &*lObj;} 
+			else if ( res2 > drabs2 ) { res2 = drabs2; lv2 = &*lObj; }
 		}
 	}
 
@@ -121,8 +110,8 @@ double Epsilon(const TNode &Node, TObject **lv, bool merge)
 
 	if ( (pt == Vortex) && merge)
 	{
-		TObject &v1 = **lv1;
-		TObject &v2 = **lv2;
+		TObj &v1 = **lv1;
+		TObj &v2 = **lv2;
 		if ( 
 			((*lv<*lv1) && (res1 < DiffMergeFast_MergeSqEps))
 			||
@@ -140,39 +129,39 @@ double Epsilon(const TNode &Node, TObject **lv, bool merge)
 
 namespace {
 template <ParticleType pt>
-void Division(const TNode &Node, const TObject &v, double _1_eps, Vector* ResP, double *ResD )
+void Division(const TNode &Node, const TObj &v, double _1_eps, TVec* ResP, double *ResD )
 {
-	Vector dr;
+	TVec dr;
 
 	ResP->zero(); *ResD = 0.;
 
-	TNode **lNNode = Node.NearNodes->First;
-	TNode **&LastNNode = Node.NearNodes->Last;
+	TNode **lNNode = Node.NearNodes->begin();
+	TNode **&LastNNode = Node.NearNodes->end();
 	for ( ; lNNode<LastNNode; lNNode++ )
 	{
 		TNode &NNode = **lNNode;
 
-		TObject **lObj;
-		TObject ***lLastObj;
+		TObj **lObj;
+		TObj ***lLastObj;
 		switch (pt)
 		{
 			case Vortex:
 				if ( !NNode.VortexLList ) { continue; }
-				lObj = NNode.VortexLList->First;
-				lLastObj = &NNode.VortexLList->Last;
+				lObj = NNode.VortexLList->begin();
+				lLastObj = &NNode.VortexLList->end();
 				break;
 			case Heat:
 				if ( !NNode.HeatLList ) { continue; }
-				lObj = NNode.HeatLList->First;
-				lLastObj = &NNode.HeatLList->Last;
+				lObj = NNode.HeatLList->begin();
+				lLastObj = &NNode.HeatLList->end();
 				break;
 		}
-		TObject **&LastObj = *lLastObj;
+		TObj **&LastObj = *lLastObj;
 
 		for ( ; lObj<LastObj; lObj++ )
 		{
 			if (!*lObj) { continue; }
-			TObject &Obj = **lObj;
+			TObj &Obj = **lObj;
 			dr = v - Obj;
 			if ( dr.iszero() ) { continue; }
 			double drabs = dr.abs();
@@ -193,10 +182,10 @@ void Division(const TNode &Node, const TObject &v, double _1_eps, Vector* ResP, 
 
 namespace {
 //inline
-void VortexInfluence(const TObject &v, const TObject &vj, double _1_eps, Vector *i2, double *i1)
+void VortexInfluence(const TObj &v, const TObj &vj, double _1_eps, TVec *i2, double *i1)
 {
 	if ( sign(v) != sign(vj) ) { return; }
-	Vector dr = v - vj;
+	TVec dr = v - vj;
 	if ( dr.iszero() ) { return; }
 	double drabs = dr.abs();
 	double exparg = -drabs*_1_eps;
@@ -208,18 +197,18 @@ void VortexInfluence(const TObject &v, const TObject &vj, double _1_eps, Vector 
 
 namespace {
 //inline
-void SegmentInfluence(const TObject &v, const TObject &pk, const TObject &pk1, 
-					double _1_eps, Vector *i3, double *i0)
+void SegmentInfluence(const TObj &v, const TObj &pk, const TObj &pk1, 
+					double _1_eps, TVec *i3, double *i0)
 {
-	Vector rk = (pk+pk1)*0.5;
+	TVec rk = (pk+pk1)*0.5;
 
-	Vector dr = v - rk;
+	TVec dr = v - rk;
 	double drabs2 = dr.abs2();
 	double drabs = sqrt( drabs2 );
 	double exparg = -drabs*_1_eps;
 	if ( exparg < ExpArgRestriction ) {return;}
 	double expres = expdef(exparg);
-	Vector dS = rotl(pk1 - pk);
+	TVec dS = rotl(pk1 - pk);
 	*i3 += dS * expres;
 	*i0 += (drabs*_1_eps+1)/drabs2*(dr*dS)*expres;
 }}
@@ -232,54 +221,56 @@ int CalcVortexDiffMergeFast()
 	double multiplier;
 	GRestriction = DiffMergeFast_S->gmax();
 
-	TList<TNode*> *BottomNodes = GetTreeBottomNodes();
+	auto BottomNodes = GetTreeBottomNodes();
 	if ( !BottomNodes ) return -1;
 
-	TNode **lBNode = BottomNodes->First;
-	TNode **&LastBNode = BottomNodes->Last;
-	for ( ; lBNode<LastBNode; lBNode++ )
+	auto lBNode = BottomNodes->begin();
+	auto lBNodeEnd = BottomNodes->end();
+	for (; lBNode<lBNodeEnd; lBNode++ )
 	{
 		TNode &BNode = **lBNode;
 		if ( !BNode.VortexLList ) { continue; }
 
-		TObject **lObj = BNode.VortexLList->First;
-		TObject **&LastObj = BNode.VortexLList->Last;
-		for ( ; lObj<LastObj; lObj++ )
+		auto vllist = BNode.VortexLList;
+		for (auto lObj = vllist->begin(); lObj<vllist->end(); lObj++ )
 		{
 			if (!*lObj) { continue; }
-			TObject &Obj = **lObj;
+			TObj &Obj = **lObj;
 
 			double eps, _1_eps;
-			eps = Epsilon<Vortex>(BNode, lObj, true);
+			eps = Epsilon<Vortex>(BNode, &*lObj, true);
 			eps = (eps > EpsRestriction) ? eps : EpsRestriction;
 			_1_eps = 1/eps;
 
-			Vector S2(0,0), S3(0,0);
+			TVec S2(0,0), S3(0,0);
 			double S1 = 0, S0 = 0;
 
-			TNode **lNNode = BNode.NearNodes->First;
-			TNode **&LastNNode = BNode.NearNodes->Last;
-			for ( ; lNNode<LastNNode; lNNode++ )
+			auto nnodes = BNode.NearNodes;
+			auto lNNode = nnodes->begin();
+			auto lNNodeEnd = nnodes->end();
+			for (; lNNode<lNNodeEnd; lNNode++ )
 			{
 				TNode &NNode = **lNNode;
 				if ( NNode.VortexLList ) {
-				TObject **lObjJ = NNode.VortexLList->First;
-				TObject **&LastObjJ = NNode.VortexLList->Last;
-				for ( ; lObjJ<LastObjJ; lObjJ++ )
+				auto jlist = NNode.VortexLList;
+				auto lObjJ = jlist->begin();
+				auto lObjJEnd = jlist->end();
+				for ( ; lObjJ<lObjJEnd; lObjJ++ )
 				{
 					if (!*lObjJ) { continue; }
-					TObject &ObjJ = **lObjJ;
+					TObj &ObjJ = **lObjJ;
 					VortexInfluence(Obj, ObjJ, _1_eps, &S2, &S1);
 				}}
 
 				if ( NNode.BodyLList ) {
-				TObject **lObjJ = NNode.BodyLList->First;
-				TObject **&LastObjJ = NNode.BodyLList->Last;
-				for ( ; lObjJ<LastObjJ; lObjJ++ )
+				auto jlist = NNode.BodyLList;
+				auto lObjJ = jlist->begin();
+				auto lObjJEnd = jlist->end();
+				for ( ; lObjJ<lObjJ; lObjJ++ )
 				{
 					if (!*lObjJ) { continue; }
-					TObject *ObjJ = *lObjJ;
-					SegmentInfluence(Obj, *ObjJ, *DiffMergeFast_S->Body->Next(ObjJ), _1_eps, &S3, &S0);
+					TObj *ObjJ = *lObjJ;
+					SegmentInfluence(Obj, *ObjJ, *DiffMergeFast_S->Body->next(vector<TObj>::const_iterator(ObjJ)), _1_eps, &S3, &S0);
 				}}
 			}
 
@@ -303,28 +294,26 @@ int CalcHeatDiffMergeFast()
 	//double multiplier2;
 	//double M_35dfi2 = 3.5 * DiffMergeFast_dfi * DiffMergeFast_dfi;
 
-	//TList<TObject> *BList = DiffMergeFast_S->Body->List;
-	TList<TNode*> *BottomNodes = GetTreeBottomNodes();
+	//TList<TObj> *BList = DiffMergeFast_S->Body->List;
+	auto BottomNodes = GetTreeBottomNodes();
 	if ( !BottomNodes ) return -1;
 
-	TNode **lBNode = BottomNodes->First;
-	TNode **&LastBNode = BottomNodes->Last;
-	for ( ; lBNode<LastBNode; lBNode++ )
+/*	for (auto lBNode = BottomNodes->begin(); lBNode<BottomNodes->end(); lBNode++ )
 	{
 		TNode &BNode = **lBNode;
 		if ( !BNode.HeatLList ) { continue; }
 
-		TObject **lObj = BNode.HeatLList->First;
-		TObject **&LastObj = BNode.HeatLList->Last;
+		TObj **lObj = BNode.HeatLList->begin();
+		TObj **&LastObj = BNode.HeatLList->end();
 		for ( ; lObj<LastObj; lObj++ )
 		{
-			TObject &Obj = **lObj;
+			TObj &Obj = **lObj;
 
 			double epsilon, eps1;
 			epsilon = Epsilon<Heat>(BNode, lObj, false);
 			eps1 = 1/epsilon;
 
-			Vector ResP;
+			TVec ResP;
 			double ResD;
 			Division<Heat>(BNode, Obj, eps1, &ResP, &ResD);
  
@@ -332,7 +321,7 @@ int CalcHeatDiffMergeFast()
 			{
 				Obj.v += ResP * (DiffMergeFast_Nyu/ResD*eps1);
 			}
-/*
+
 			if ( BList )
 			{
 				double rabs = sqrtdef(Obj.rx*Obj.rx + Obj.ry*Obj.ry);
@@ -346,10 +335,10 @@ int CalcHeatDiffMergeFast()
 					Obj.vy += Obj.ry * multiplier2;
 				}
 			}
-*/
+
 		}
 	}
-
+*/
 	return 0;
 }
 
