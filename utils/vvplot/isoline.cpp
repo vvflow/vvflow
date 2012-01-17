8e46f5343df650e7394bf7f760a97166f3238659
Yaroslav Dynnikov <rosik.ru@gmail.com>
2012-01-17 08:07:35 +0400

    Optimized isolines
    Former-commit-id: 88c2200c39bae38894fe34039a89479967b20eb2

--- utils/vvplot/isoline.cpp
+++ utils/vvplot/isoline.cpp
@@ -1,121 +1,167 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "iostream"
 #include "malloc.h"
+#include <time.h>
 
-#include "core.h"
+//#include "core.h"
 using namespace std;
 
-class dot
-{
-	public:
-	dot() {x=y=z=0;}
-	dot(double _x, double _y, double _z) {x=_x; y=_y; z=_z;}
-	friend bool operator == (dot &p1, dot &p2) { return (p1.x==p2.x)&&(p1.y==p2.y); }
+#define Panic {cerr << "Bad file!\n"; return -3;}
 
-	double x, y, z;
-};
+struct vec { double x, y; };
+typedef struct vec Vec;
 
-class seg
-{
-	public:
-	seg() {}
-	seg(dot _p1, dot _p2) {p1=_p1; p2=_p2;}
-	dot p1, p2;
-};
+double max(double a1, double a2) {return (a1>a2)?a1:a2;}
+double min(double a1, double a2) {return (a1<a2)?a1:a2;}
 
-//double max(double a1, double a2) {return (a1>a2)?a1:a2;}
-//double min(double a1, double a2) {return (a1<a2)?a1:a2;}
-bool between(double c, double a1, double a2, double a3)
+void ProcessRect(double x1, double x2, double y1, double y2, double z11,
+				double z12, double z21, double z22, double C)
 {
-	if (!a1 || !a2 || !a3) return false;
-	return (c<max(a1, max(a2, a3)))&&(c>min(a1, min(a2, a3)));
-}
+	Vec vecs[4];
+	int N=0;
 
-bool between(double c, dot p1, dot p2, dot& res)
-{
-	if ((c<=max(p1.z, p2.z))&&(c>=min(p1.z, p2.z)))
+	if ( (z11<C && C<z12) || (z12<C && C<z11) )
 	{
-		double tmp = (c-p1.z)/(p2.z-p1.z);
-		res.x = p1.x + (p2.x-p1.x)*tmp;
-		res.y = p1.y + (p2.y-p1.y)*tmp;
-		return true;
+		vecs[N].x = x1;
+		vecs[N].y = y1 + (C-z11)*(y2-y1)/(z12-z11);
+		N++;
 	}
-	return false;
+
+	if ( (z22<C && C<z21) || (z21<C && C<z22) )
+	{
+		vecs[N].x = x2;
+		vecs[N].y = y1 + (C-z21)*(y2-y1)/(z22-z21);
+		N++;
+	}
+
+	if ( (z12<C && C<z22) || (z22<C && C<z12) )
+	{
+		vecs[N].x = x1 + (C-z12)*(x2-x1)/(z22-z12);
+		vecs[N].y = y2;
+		N++;
+	}
+
+	if ( (z21<C && C<z11) || (z11<C && C<z21) )
+	{
+		vecs[N].x = x1 + (C-z11)*(x2-x1)/(z21-z11);
+		vecs[N].y = y1;
+		N++;
+	}
+
+	if (N==1 || N==3) {cerr << "Fanatactic!\n"; exit(-4); }
+	if (N==2)
+	{
+		//#pragma omp critical(print)
+		{
+			printf("%lg\t%lg\n", vecs[0].x, vecs[0].y);
+			printf("%lg\t%lg\n", vecs[1].x, vecs[1].y);
+			printf("\n");
+		}
+	}
+	if (N==4)
+	{
+		//#pragma omp critical(print)
+		{
+			printf("%lg\t%lg\n", vecs[2].x, vecs[2].y);
+			printf("%lg\t%lg\n", vecs[3].x, vecs[3].y);
+			printf("\n");
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
-	if ( argc != 8)\
+	time_t begining = clock();
+	if ( argc < 3 )
 	{
-		fprintf(stderr, "Error! Please use: \nisoline_exe file.map const prec xmin xmax ymin ymax\n");
+		fprintf(stderr, "Error! Please use: \nisoline_exe file.map const\n");
 		return -1;
 	}
 
 	double C = atof(argv[2]);
-	double xmin, xmax, ymin, ymax, prec;
-	{ int i=3;
-		xmin = atof(argv[++i]);
-		xmax = atof(argv[++i]);
-		ymin = atof(argv[++i]);
-		ymax = atof(argv[++i]);
-		prec = atof(argv[3]); }
-	int imax = (xmax-xmin)/prec + 1;
-	int jmax = (ymax-ymin)/prec + 1;
-
 	FILE* fin = fopen(argv[1], "r");
 	if (!fin) { perror("Error"); return 0; }
 
-	double xaxis[imax], yaxis[jmax], data[imax][jmax];
+	double *xaxis, *yaxis, *data;
+	xaxis = (double*) malloc(sizeof(double));
+	yaxis = (double*) malloc(sizeof(double));
+	data = (double*) malloc(sizeof(double));
 
-	double x, y, z; int k=0;
-	while ( fscanf(fin, "%lf %lf %lf", &x, &y, &z)==3 )
+	double x, y, z;
+	int imax=0, jmax=0;
+
+	if (fscanf(fin, "%lf %lf %lf", &x, &y, &z) != 3) Panic;
+	xaxis[0] = x;
+	yaxis[0] = y;
+	data[0] = z;
+
+	while(true)
 	{
-		//realloc(xaxis, imax*sizeof(double));
-		xaxis[k/jmax] = x;
-		yaxis[k%jmax] = y;
-		data[k/jmax][k%jmax] = z;
-		k++;
+		if (fscanf(fin, "%lf %lf %lf", &x, &y, &z) != 3) Panic;
+		if (xaxis[0] != x) break;
+
+		jmax++;
+		yaxis = (double*)realloc(yaxis, (jmax+1)*sizeof(double));
+		data = (double*)realloc(data, (jmax+1)*sizeof(double));
+		yaxis[jmax] = y;
+		data[jmax] = z;
 	}
 
-	vector<seg> *seglist = new vector<seg>();
-	for (int i=0; i<(imax-1); i++)
+	bool go = true;
+	while(go)
 	{
-		#pragma omp parallel for
-		for (int j=0; j<(jmax-1); j++)
+		for (int j=0; j<=jmax; j++)
 		{
-			if (between(C, data[i][j], data[i][j+1], data[i+1][j+1]))
+			if (yaxis[j] != y) Panic;
+			if (!j)
 			{
-				dot p1(xaxis[i], yaxis[j], data[i][j]);
-				dot p2(xaxis[i], yaxis[j+1], data[i][j+1]);
-				dot p3(xaxis[i+1], yaxis[j+1], data[i+1][j+1]);
-				dot pn[2];
-				int i=0;
-				if (between(C, p1, p2, pn[i])) i++;
-				if (between(C, p2, p3, pn[i])) i++;
-				if (between(C, p3, p1, pn[i])) i++;
-
-				#pragma omp critical
-				seglist->push_back(seg(pn[0], pn[1]));
+				imax++;
+				xaxis = (double*) realloc(xaxis, (imax+1)*sizeof(double));
+				data = (double*) realloc(data, (jmax+1)*(imax+1)*sizeof(double));
+				xaxis[imax] = x;
 			}
-			if (between(C, data[i][j], data[i+1][j], data[i+1][j+1]))
-			{
-				dot p1(xaxis[i], yaxis[j], data[i][j]);
-				dot p2(xaxis[i+1], yaxis[j], data[i+1][j]);
-				dot p3(xaxis[i+1], yaxis[j+1], data[i+1][j+1]);
-				dot pn[2];
-				int i=0;
-				if (between(C, p1, p2, pn[i])) i++;
-				if (between(C, p2, p3, pn[i])) i++;
-				if (between(C, p3, p1, pn[i])) i++;
 
-				#pragma omp critical
-				seglist->push_back(seg(pn[0], pn[1]));
+			data[(jmax+1)*imax + j] = z;
+
+			if (fscanf(fin, "%lf %lf %lf", &x, &y, &z) != 3)
+				if (j==jmax) {go=false;}
+				else Panic;
+		}
+	}
+
+	/*for(int i=0; i<=imax; i++)
+	for(int j=0; j<=jmax; j++)
+	{
+		cout << xaxis[i] << "\t" << yaxis[j] << "\t" << data[i*(jmax+1)+j] << endl;
+	}*/
+
+	//return 0;
+	double steptime = double(clock()-begining)/CLOCKS_PER_SEC;
+
+	begining = clock();
+//	seglist = new vector<seg>();
+	for (int c=2; c<argc; c++)
+	{
+		double C = atof(argv[c]);
+		for (int i=0; i<=(imax-1); i++)
+		{
+			//#pragma omp parallel for
+			for (int j=0; j<=(jmax-1); j++)
+			{
+				ProcessRect(xaxis[i], xaxis[i+1], yaxis[j], yaxis[j+1],
+							data[i*(jmax+1)+j], data[i*(jmax+1)+j+1],
+							data[(i+1)*(jmax+1)+j], data[(i+1)*(jmax+1)+j+1], C);
 			}
 		}
 	}
 
+	printf("0\t0\n0\t0\n");
+
+/*
+	cerr << seglist->size_safe() << endl;
 	if (!seglist->size_safe()) {printf("0 0\n0 0\n"); return 0;}
-	dot end = seglist->begin()->p1;
+	Vec end = seglist->begin()->v1;
 	#define print printf("%lg\t%lg\n", end.x, end.y)
 	print;
 	while (seglist->size())
@@ -124,16 +170,16 @@ int main(int argc, char **argv)
 		//FIXME this loop doesnt work with -O3
 		const_for (seglist, lseg)
 		{
-			if (lseg->p1 == end)
+			if (lseg->v1.x == end.x && lseg->v1.y == end.y)
 			{
-				end = lseg->p2;
+				end = lseg->v2;
 				seglist->erase(lseg);
 				updated = true;
 				break;
 			} else
-			if (lseg->p2 == end)
+			if (lseg->v2.x == end.x && lseg->v2.y == end.y)
 			{
-				end = lseg->p1;
+				end = lseg->v1;
 				seglist->erase(lseg);
 				updated = true;
 				break;
@@ -142,13 +188,16 @@ int main(int argc, char **argv)
 
 		if (!updated)
 		{
-			end = seglist->begin()->p1;
+			end = seglist->begin()->v1;
 			printf("\n");
 		}
 
 		print;
 	}
 	#undef print
+	*/
+	steptime = double(clock()-begining)/CLOCKS_PER_SEC;
+	//cerr << "Calc: " << steptime << endl;
 
 	return 0;
 }
