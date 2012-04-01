e7e05121ceafda867b1d97ce664a7b5ff658d74f
Yaroslav Dynnikov <rosik.ru@gmail.com>
2012-04-01 15:34:29 +0400

    Improved vvcompose
    Former-commit-id: d8b97f7993040444a029f7349d2e82791f3ccf42

--- utils/vvplot/vvcompose.cpp
+++ utils/vvplot/vvcompose.cpp
@@ -6,17 +6,25 @@
 
 #include "core.h"
 
+double Vx, Vy;
+TVec InfSpeed(double t)
+{
+	return TVec(Vx, Vy);
+}
+
 int main(int argc, char *argv[])
 {
 	if ( argc < 3)\
 	{
-		cerr << "Error! Please use: \vvcompose_exe -[bhov] file\n" <<
+		cerr << "Error! Please use: vvcompose_exe -[bhov] file [-x Vx]" <<
+		        " [-y Vy] [-t Time] [-r 1/nyu]\n" <<
 		        "-b{2,3,5} - body file with 2 3 or 5 columns\n" <<
 		        "-h - heat file\n-o - output\n-v - vortexes\n";
 		return -1;
 	}
 
-	Space *S = new Space();
+	Space *S = new Space(InfSpeed);
+	Vx = Vy = 0;
 	char *output;
 	for (int i=1; i<argc; i+=2)
 	{
@@ -27,6 +35,10 @@ int main(int argc, char *argv[])
 			case 'v': S->LoadVorticityFromFile(argv[i+1]); break;
 			case 'h': S->LoadHeatFromFile(argv[i+1]); break;
 			case 'o': output = argv[i+1]; break;
+			case 'x': Vx = atof(argv[i+1]); break;
+			case 'y': Vy = atof(argv[i+1]); break;
+			case 't': S->Time = atof(argv[i+1]); break;
+			case 'r': S->Re = atof(argv[i+1]); break;
 			default: cerr << "Bad option: -" << argv[i][1] << endl; return -1;
 		}
 	}
