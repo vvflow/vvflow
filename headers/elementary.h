16fab8d5ad7f68a69d51b875a9528aba495ff01c
Yaroslav Dynnikov <rosik@rlap>
2013-03-14 22:57:56 +0400

    Sleaky update of TVec and TObj
    Former-commit-id: 7edc50aa4b4b7ba7a710b257747f2c81b1ded2c6

--- headers/elementary.h
+++ headers/elementary.h
@@ -30,71 +30,68 @@ inline double max(double a, double b, double c) { return max(a, max(b, c)); }
 inline double min(double a, double b, double c) { return min(a, min(b, c)); }
 inline double max(double a, double b, double c, double d) { return max(a, max(b, max(c,d))); }
 inline double min(double a, double b, double c, double d) { return min(a, min(b, min(c,d))); }
-inline bool divisible(double dividend, double divisor, double precision) {
-	return (fabs(dividend-int(dividend/divisor+0.5)*divisor) < precision);
-	// divisible = кратно
-	// dividend = делимое
-	// divisor = делитель
-	// precision = точность
-	// возвращает true если первый аргумент кратен второму с заданной точностью
-}
-inline bool divisible(double dividend, double divisor)
-{
-	return divisible(dividend, divisor, divisor*0.01);
-}
 
 class TVec
 {
 	public:
-		double rx, ry;
+		double x, y;
 
-		TVec() {}
-		TVec(double rx_, double ry_) {rx=rx_; ry=ry_;}
+	public:
+		TVec()
+			{x = y = 0.;}
+		TVec(double rx, double ry)
+			{x=rx; y=ry;}
 
-		double abs() const {return sqrt(rx*rx+ry*ry);}
-		double abs2() const {return rx*rx+ry*ry;}
-		bool iszero() const {return (fabs(rx)+fabs(ry) < 1E-10); }
-		void zero() { rx = ry = 0; }
-		void init(double rx_, double ry_) {rx=rx_; ry=ry_;}
+		double abs() const {return sqrt(x*x+y*y);}
+		double abs2() const {return x*x+y*y;}
+		bool iszero() const {return (fabs(x)+fabs(y) < 1E-10); }
+		void set(double rx, double ry) {x=rx; y=ry;}
 
-		friend istream& operator>> (istream& is, TVec& p) 		{ return is >> p.rx >> p.ry; }
-		friend ostream& operator<< (ostream& os, const TVec& p) 	{ return os << p.rx << " \t" << p.ry; }
+		friend istream& operator>> (istream& is, TVec& p) 		{ return is >> p.x >> p.y; }
+		friend ostream& operator<< (ostream& os, const TVec& p) 	{ return os << p.x << " \t" << p.y; }
 
-		friend const TVec operator* (const double c, const TVec &p) { return TVec(p.rx*c, p.ry*c); }
-		friend const TVec operator* (const TVec &p, const double c) { return TVec(p.rx*c, p.ry*c); }
-		friend const TVec operator/ (const TVec &p, const double c) { return p*(1/c); }
-		friend const TVec operator+ (const TVec &p1, const TVec &p2) { return TVec(p1.rx+p2.rx, p1.ry+p2.ry); }
-		friend const TVec operator- (const TVec &p1, const TVec &p2) { return TVec(p1.rx-p2.rx, p1.ry-p2.ry); }
+		friend const TVec operator* (const double c, const TVec &p) { return TVec(p.x*c, p.y*c); }
+		friend const TVec operator* (const TVec &p, const double c) { return TVec(p.x*c, p.y*c); }
+		friend const TVec operator/ (const TVec &p, const double c) { return p*(1./c); }
+		friend const TVec operator+ (const TVec &p1, const TVec &p2) { return TVec(p1.x+p2.x, p1.y+p2.y); }
+		friend const TVec operator- (const TVec &p1, const TVec &p2) { return TVec(p1.x-p2.x, p1.y-p2.y); }
 
-		friend void operator+= (TVec &p1, const TVec& p2) { p1.rx+= p2.rx; p1.ry+= p2.ry; }
-		friend void operator-= (TVec &p1, const TVec& p2) { p1.rx-= p2.rx; p1.ry-= p2.ry; }
-		friend void operator*= (TVec &p, const double c) { p.rx*= c; p.ry*= c; }
-		friend void operator/= (TVec &p, const double c) { p*= (1/c); }
-		friend bool operator!= (const TVec &p1, const TVec &p2) { return ((p1.rx!=p2.rx)||(p1.ry!=p2.ry)); }
+		friend void operator+= (TVec &p1, const TVec& p2) { p1.x+= p2.x; p1.y+= p2.y; }
+		friend void operator-= (TVec &p1, const TVec& p2) { p1.x-= p2.x; p1.y-= p2.y; }
+		friend void operator*= (TVec &p, const double c) { p.x*= c; p.y*= c; }
+		friend void operator/= (TVec &p, const double c) { p*= (1./c); }
+		friend bool operator!= (const TVec &p1, const TVec &p2) { return ((p1.x!=p2.x)||(p1.y!=p2.y)); }
 
-		friend double operator* (const TVec &p1, const TVec &p2) 	{ return p1.rx*p2.rx + p1.ry*p2.ry; }
-		friend const TVec rotl(const TVec &p) { return TVec(-p.ry, p.rx); }
+		friend double operator* (const TVec &p1, const TVec &p2) 	{ return p1.x*p2.x + p1.y*p2.y; }
+		friend const TVec rotl(const TVec &p) { return TVec(-p.y, p.x); }
 		//NB: double * rotl(vec) = (in terms of math) (e_z*double) \times vec
-		friend const TVec operator- (const TVec &p) { return TVec(-p.rx, -p.ry); }
+		friend const TVec operator- (const TVec &p) { return TVec(-p.x, -p.y); }
 };
 
-class TObj: public TVec
+class TObj
 {
 	public:
+		TVec r;
 		double g;
 		TVec v;
+	public:
+		const double &rx, &ry, &vx, &vy;
 		double _1_eps; // eps = max (nearest dl/3 on wall,  dist to 2nd nearest vortex)
 
-		TObj() {}
-		TObj(double rx_, double ry_, double g_):TVec(rx_, ry_) { g=g_; v.zero();}
-		TObj(TVec r_, double g_){rx=r_.rx; ry=r_.ry; g=g_; v.zero();}
+		TObj()
+			:rx(r.x), ry(r.y), vx(v.x), vy(v.y), r(), v()
+			{g = _1_eps = 0.;}
+		TObj(double rx_, double ry_, double g_)
+			:rx(r.x), ry(r.y), vx(v.x), vy(v.y), r(rx_, ry_), v()
+			{ g=g_; _1_eps = 0.;}
+		TObj(TVec r_, double g_)
+			:rx(r.x), ry(r.y), vx(v.x), vy(v.y), r(r_), v()
+			{ g=g_; _1_eps = 0.;}
 
-		void zero() { rx = ry = g = 0; v.zero(); }
-		void init(double rx_, double ry_, double g_) {rx=rx_; ry=ry_; g=g_;}
-		short sign() { return ::sign(g); }
+		int sign() { return ::sign(g); }
 
-		TObj& operator= (const TVec& p) { rx=p.rx; ry=p.ry; return *this; }
-		friend istream& operator>> (istream& is, TObj& p) 		{ return is >> p.rx >> p.ry >> p.g; }
+		//TObj& operator= (const TVec& p) { rx=p.rx; ry=p.ry; return *this; }
+		friend istream& operator>> (istream& is, TObj& p) 		{ return is >> p.r.x >> p.r.y >> p.g; }
 		friend ostream& operator<< (ostream& os, const TObj& p) 	{ return os << p.rx << " \t" << p.ry << " \t" << p.g; }
 		friend short sign(const TObj& p) { return ::sign(p.g); }
 };
