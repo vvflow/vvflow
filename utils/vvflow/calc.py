70891f9b3fac39c7031a14a6f2d1642d8ed66d7e
Yaroslav Dynnikov <rosik@rlap>
2014-06-03 15:53:32 +0400

    [add] line function
    Former-commit-id: 6fafb98262652cfd7b67a41868d32ec8fbc100d6

--- utils/vvflow/calc.py
+++ utils/vvflow/calc.py
@@ -8,6 +8,16 @@ if len(argv) > 1:
 	for i in argv[1:]:
 		allargs+= i+" "
 
+def line(line, x):
+	line.sort()
+	#find approximation segment
+	try:
+		p1 = next(p for p in reversed(line) if p[0] < x)
+		p2 = next(p for p in line if p[0] >= x)
+		return p1[1]+float(x-p1[0])*float(p2[1]-p1[1])/float(p2[0]-p1[0])
+	except:
+		return 0
+
 try: print eval(allargs);
 except SyntaxError: print 0; exit();
 except NameError: print 0; exit();
