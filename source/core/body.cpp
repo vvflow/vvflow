f0900a04eec52a1f9a48e4564a81a365b66fc4a0
Yaroslav Dynnikov <rosik@rlap>
2015-04-19 03:16:13 +0300

    [fix] prevent bodies from spreading out
    Former-commit-id: 92a797586d3c3ef362afe6d39cf03b351501894c

--- source/core/body.cpp
+++ source/core/body.cpp
@@ -7,200 +7,200 @@
 using namespace std;
 
 TBody::TBody(Space *space):
-	alist(),
-	speed_x(),
-	speed_y(),
-	speed_o()
+    alist(),
+    speed_x(),
+    speed_y(),
+    speed_o()
 {
-	S = space;
+    S = space;
 
-	holder = dpos = TVec3D(0., 0., 0.);
-	g_dead = 0;
-	friction = friction_prev = TVec3D(0,0,0);
-	force_born = force_dead = TVec3D(0,0,0);
-	force_hydro = force_holder = TVec3D(0,0,0);
-	_surface = _area = 0;
-	_com = TVec(0., 0.);
-	_moi_c = _moi_com = 0;
-	kspring = TVec3D(-1., -1., -1.);
-	damping = TVec3D(0., 0., 0.);
-	density = 1.;
-	special_segment_no = 0;
-	boundary_condition = bc_t::steady;
-	heat_condition = hc_t::neglect;
+    holder = dpos = TVec3D(0., 0., 0.);
+    g_dead = 0;
+    friction = friction_prev = TVec3D(0,0,0);
+    force_born = force_dead = TVec3D(0,0,0);
+    force_hydro = force_holder = TVec3D(0,0,0);
+    _surface = _area = 0;
+    _com = TVec(0., 0.);
+    _moi_c = _moi_com = 0;
+    kspring = TVec3D(-1., -1., -1.);
+    damping = TVec3D(0., 0., 0.);
+    density = 1.;
+    special_segment_no = 0;
+    boundary_condition = bc_t::steady;
+    heat_condition = hc_t::neglect;
 }
 
 int TBody::get_index() const
 {
-	int i = 0;
-	for (auto lbody: S->BodyList)
-	{
-		if (lbody.get() == this) return i;
-		i++;
-	}
-	return -1;
+    int i = 0;
+    for (auto lbody: S->BodyList)
+    {
+        if (lbody.get() == this) return i;
+        i++;
+    }
+    return -1;
 }
 
 std::string TBody::get_name() const
 {
-	char name[8];
-	sprintf(name, "body%02d", get_index());
-	return std::string(name);
+    char name[8];
+    sprintf(name, "body%02d", get_index());
+    return std::string(name);
 }
 
 
 TVec3D TBody::get_speed() const
 {
-	return TVec3D(
-	              speed_x.getValue(S->Time),
-	              speed_y.getValue(S->Time),
-	              speed_o.getValue(S->Time)
-	             );
+    return TVec3D(
+            speed_x.getValue(S->Time),
+            speed_y.getValue(S->Time),
+            speed_o.getValue(S->Time)
+            );
 }
 
 void TBody::doRotationAndMotion()
 {
-	doRotation();
-	doMotion();
-	doUpdateSegments();
-	doFillProperties();
+    doRotation();
+    doMotion();
+    doUpdateSegments();
+    doFillProperties();
 }
 
 void TBody::doRotation()
 {
-	auto root_body = this->root_body.lock();
-	double angle_root = root_body ?
-	                  S->dt * root_body->speed_slae.o
-	                  : 0;
-	const double angle_slae = speed_slae.o * S->dt; //in doc \omega_? \Delta t
-	const double angle_solid = get_speed().o * S->dt; //in doc \omega \Delta t
-	for (auto& att: alist)
-	{
-		TVec dr = att.corner - get_axis();
-		att.corner = get_axis() + dr*cos(angle_slae) + rotl(dr)*sin(angle_slae);
-	}
-	holder.o += angle_solid + angle_root;
-	dpos.o += angle_slae - angle_solid - angle_root;
-	speed_slae_prev.o = speed_slae.o;
+    const double angle_slae = speed_slae.o * S->dt; //in doc \omega_? \Delta t
+    const double angle_solid = get_speed().o * S->dt; //in doc \omega \Delta t
+    for (auto& att: alist)
+    {
+        TVec dr = att.corner - get_axis();
+        att.corner = get_axis() + dr*cos(angle_slae) + rotl(dr)*sin(angle_slae);
+    }
+
+    auto root_body = this->root_body.lock();
+    double angle_root = !root_body ? 0 : S->dt * root_body->speed_slae.o;
+    holder.o += angle_solid + angle_root;
+    dpos.o += angle_slae - angle_solid - angle_root;
+    speed_slae_prev.o = speed_slae.o;
 }
 
 void TBody::doMotion()
 {
-	auto root_body = this->root_body.lock();
-	TVec delta_root = root_body ? 
-	                  S->dt * (root_body->speed_slae.r +
-	                  root_body->speed_slae.o * rotl(get_axis() - root_body->get_axis()))
-	                  : TVec(0,0);
-	TVec delta_slae = speed_slae.r * S->dt;
-	TVec delta_solid = get_speed().r * S->dt;
-	for (auto& obj: alist)
-	{
-		obj.corner += delta_slae;
-	}
-	holder.r += delta_solid + delta_root;
-	dpos.r += delta_slae - delta_solid - delta_root;
-	speed_slae_prev.r = speed_slae.r;
+    TVec delta_slae = speed_slae.r * S->dt;
+    TVec delta_solid = get_speed().r * S->dt;
+    for (auto& obj: alist)
+    {
+        obj.corner += delta_slae;
+    }
+    auto root_body = this->root_body.lock();
+    TVec delta_root(0, 0);
+    if (root_body)
+    {
+        TVec dr = holder.r - root_body->get_axis();
+        double da = S->dt * root_body->speed_slae.o;
+        delta_root = S->dt * root_body->speed_slae.r + dr*cos(da) + rotl(dr)*sin(da);
+    }
+
+    holder.r += delta_solid + delta_root;
+    dpos.r += delta_slae - delta_solid - delta_root;
+    speed_slae_prev.r = speed_slae.r;
 }
 
 void TBody::doUpdateSegments()
 {
-	if (!this) return;
+    alist.push_back(alist.front());
 
-	alist.push_back(alist.front());
-
-	for (auto lobj=alist.begin(); lobj<alist.end()-1; lobj++)
-	{
-		lobj->dl = (lobj+1)->corner - lobj->corner;
-		lobj->_1_eps = 3.0/lobj->dl.abs();
-		lobj->r = 0.5*((lobj+1)->corner + lobj->corner);
-	}
-	alist.pop_back();
+    for (auto lobj=alist.begin(); lobj<alist.end()-1; lobj++)
+    {
+        lobj->dl = (lobj+1)->corner - lobj->corner;
+        lobj->_1_eps = 3.0/lobj->dl.abs();
+        lobj->r = 0.5*((lobj+1)->corner + lobj->corner);
+    }
+    alist.pop_back();
 }
 
 TAtt* TBody::isPointInvalid(TVec p)
 {
-	return isPointInContour(p, alist);
+    return isPointInContour(p, alist);
 }
 
 TAtt* TBody::isPointInHeatLayer(TVec p)
 {
-	if (!heat_layer.size())
-	{
-		for (auto& lobj: alist)
-		{
-			heat_layer.push_back(lobj.r + rotl(lobj.dl));
-		}
-	}
+    if (!heat_layer.size())
+    {
+        for (auto& lobj: alist)
+        {
+            heat_layer.push_back(lobj.r + rotl(lobj.dl));
+        }
+    }
 
-	return isPointInContour(p, heat_layer);
+    return isPointInContour(p, heat_layer);
 }
 
 template <class T> TVec corner(T lobj);
 template <> TVec corner(TVec lobj) {return lobj;}
 template <> TVec corner(TAtt lobj) {return lobj.corner;}
-
 template <class T>
 TAtt* TBody::isPointInContour(TVec p, vector<T> &list)
 {
-	bool inContour = isInsideValid();
-	TAtt *nearest = NULL;
-	double nearest_dr2 = std::numeric_limits<double>::max();
+    bool inContour = isInsideValid();
+    TAtt *nearest = NULL;
+    double nearest_dr2 = std::numeric_limits<double>::max();
 
-	for (auto i = list.begin(), j = list.end()-1; i<list.end(); j=i++)
-	{
-		TVec vi = corner<T>(*i);
-		TVec vj = corner<T>(*j);
+    for (auto i = list.begin(), j = list.end()-1; i<list.end(); j=i++)
+    {
+        TVec vi = corner<T>(*i);
+        TVec vj = corner<T>(*j);
 
-		if ((
-			(vi.y < vj.y) && (vi.y < p.y) && (p.y <= vj.y) &&
-			((vj.y - vi.y) * (p.x - vi.x) > (vj.x - vi.x) * (p.y - vi.y))
-			) || (
-			(vi.y > vj.y) && (vi.y > p.y) && (p.y >= vj.y) &&
-			((vj.y - vi.y) * (p.x - vi.x) < (vj.x - vi.x) * (p.y - vi.y))
-		)) inContour = !inContour;
-	}
+        if ((
+                    (vi.y < vj.y) && (vi.y < p.y) && (p.y <= vj.y) &&
+                    ((vj.y - vi.y) * (p.x - vi.x) > (vj.x - vi.x) * (p.y - vi.y))
+            ) || (
+                (vi.y > vj.y) && (vi.y > p.y) && (p.y >= vj.y) &&
+                ((vj.y - vi.y) * (p.x - vi.x) < (vj.x - vi.x) * (p.y - vi.y))
+                )) inContour = !inContour;
+    }
 
-	if (!inContour) return NULL;
+    if (!inContour) return NULL;
 
-	// FIXME почитать алгоритмы, поискать оптимизированный поиск минимума
-	for (auto& latt: alist)
-	{
-		double dr2 = (latt.r - p).abs2();
-		if ( dr2 < nearest_dr2 )
-		{
-			nearest = &latt;
-			nearest_dr2 = dr2;
-		}
-	}
+    // FIXME почитать алгоритмы, поискать оптимизированный поиск минимума
+    for (auto& latt: alist)
+    {
+        double dr2 = (latt.r - p).abs2();
+        if ( dr2 < nearest_dr2 )
+        {
+            nearest = &latt;
+            nearest_dr2 = dr2;
+        }
+    }
 
-	return nearest;
+    return nearest;
 }
 
 void TBody::doFillProperties()
 {
-	// FIXME mistake: moi_c after load and save changes
-	_surface = _area = _moi_c = _moi_com = 0;
-	TVec _3S_com= TVec(0., 0.);
-	double _12moi_0 = 0.;
+    // FIXME mistake: moi_c after load and save changes
+    _surface = _area = _moi_c = _moi_com = 0;
+    TVec _3S_com= TVec(0., 0.);
+    double _12moi_0 = 0.;
 
-	alist.push_back(alist.front());
-	for (auto latt=alist.begin(); latt<alist.end()-1; latt++)
-	{
-		_surface+= latt->dl.abs();
-		_area+= latt->r.y*latt->dl.x;
-		_3S_com-= latt->r * (rotl(latt->corner) * (latt+1)->corner);
-		_12moi_0 -= (latt->corner.abs2() + latt->corner*(latt+1)->corner + (latt+1)->corner.abs2())
-		            *  (rotl(latt->corner) * (latt+1)->corner);
-	
-	}
-	alist.pop_back();
-	_com = _3S_com/(3*_area);
-	_moi_com = _12moi_0/12. - _area*_com.abs2();
-	_moi_c = _moi_com + _area*(get_axis() - _com).abs2();
+    alist.push_back(alist.front());
+    for (auto latt=alist.begin(); latt<alist.end()-1; latt++)
+    {
+        _surface+= latt->dl.abs();
+        _area+= latt->r.y*latt->dl.x;
+        _3S_com-= latt->r * (rotl(latt->corner) * (latt+1)->corner);
+        _12moi_0 -= (latt->corner.abs2() + latt->corner*(latt+1)->corner + (latt+1)->corner.abs2())
+            *  (rotl(latt->corner) * (latt+1)->corner);
+
+    }
+    alist.pop_back();
+    _com = _3S_com/(3*_area);
+    _moi_com = _12moi_0/12. - _area*_com.abs2();
+    _moi_c = _moi_com + _area*(get_axis() - _com).abs2();
 }
 
 inline double atan2(const TVec &p)
 {
-	return atan2(p.y, p.x);
+    return atan2(p.y, p.x);
 }
 
--- source/modules/flowmove.cpp
+++ source/modules/flowmove.cpp
@@ -7,204 +7,211 @@
 
 flowmove::flowmove(Space *sS, double sRemoveEps)
 {
-	S = sS;
-	dt = S->dt;
-	RemoveEps = sRemoveEps;
-	CleanedV_ = 0;
+    S = sS;
+    dt = S->dt;
+    RemoveEps = sRemoveEps;
+    CleanedV_ = 0;
 }
 
 void flowmove::MoveAndClean(bool remove, bool zero_speed)
 {
-	CleanedV_ = 0;
-	S->InfMarker+= S->InfSpeed()*S->dt;
+    CleanedV_ = 0;
+    S->InfMarker+= S->InfSpeed()*S->dt;
+
+    // Move bodies
+    // to prevent bodies from separating, fix tangential velocities
+    for (auto& lbody: S->BodyList)
+    {
+        auto root = lbody->root_body.lock();
+        if (!root) continue;
+        double da = root->speed_slae.o * dt;
+        TVec dr = lbody->holder.r - root->get_axis();
+        lbody->speed_slae.r -= rotl(dr)*da - (dr*cos(da)+rotl(dr)*sin(da));
+    }
+    for (auto& lbody: S->BodyList) lbody->doRotationAndMotion();
+    // Move vortexes, heat, ink
+    for (auto& lobj: S->VortexList) lobj.r += lobj.v * dt;
+    for (auto& lobj: S->HeatList)   lobj.r += lobj.v * dt;
+    for (auto& lobj: S->StreakList) lobj.r += lobj.v * dt;
+
+    // Remove small vortexes, small heat
+    // FIXME попробовать переписать с std::remoe_if() и лямбдой
+    for (auto lobj = S->VortexList.begin(); lobj < S->VortexList.end(); )
+    {
+        if ( fabs(lobj->g) < RemoveEps ) S->VortexList.erase(lobj);
+        else lobj++;
+    }
+    for (auto lobj = S->HeatList.begin(); lobj < S->HeatList.end(); )
+    {
+        if ( fabs(lobj->g) < RemoveEps ) S->HeatList.erase(lobj);
+        else lobj++;
+    }
 
 
 
-	// Move bodies
-	for (auto& lbody: S->BodyList) lbody->doRotationAndMotion();
-	// Move vortexes, heat, ink
-	for (auto& lobj: S->VortexList) lobj.r += lobj.v * dt;
-	for (auto& lobj: S->HeatList)   lobj.r += lobj.v * dt;
-	for (auto& lobj: S->StreakList) lobj.r += lobj.v * dt;
 
-	// Remove small vortexes, small heat
-	// FIXME попробовать переписать с std::remoe_if() и лямбдой
-	for (auto lobj = S->VortexList.begin(); lobj < S->VortexList.end(); )
-	{
-		if ( fabs(lobj->g) < RemoveEps ) S->VortexList.erase(lobj);
-		else lobj++;
-	}
-	for (auto lobj = S->HeatList.begin(); lobj < S->HeatList.end(); )
-	{
-		if ( fabs(lobj->g) < RemoveEps ) S->HeatList.erase(lobj);
-		else lobj++;
-	}
+    if (remove)
+        for (auto lobj = S->VortexList.begin(); lobj < S->VortexList.end(); )
+        {
+            TAtt* bad_segment = NULL;
+            TBody* bad_body = NULL;
+            for (auto& lbody: S->BodyList)
+            {
+                bad_segment = lbody->isPointInvalid(lobj->r);
+                if (bad_segment) { bad_body = lbody.get(); break; }
+            }
+            if (!bad_body) { lobj++; continue; }
 
+            bad_body->force_dead.r += rotl(lobj->r) * lobj->g;
+            bad_body->force_dead.o += (lobj->r - bad_body->get_axis()).abs2() * lobj->g;
+            bad_segment->gsum -= lobj->g;
+            bad_body->g_dead += lobj->g;
+            CleanedV_++;
+            S->VortexList.erase(lobj);
+        }
 
+    for (auto lobj = S->HeatList.begin(); lobj < S->HeatList.end(); )
+    {
+        TAtt* bad_segment = NULL;
+        TBody* bad_body = NULL;
+        // check if particle is in body
+        for (auto& lbody: S->BodyList)
+        {
+            bad_segment = lbody->isPointInvalid(lobj->r);
+            if (bad_segment) { bad_body = lbody.get(); break; }
+        }
 
+        if (bad_body)
+        {
+            bad_segment->hsum -= lobj->g;
+            S->HeatList.erase(lobj);
+            continue;
+        }
 
-	if (remove)
-	for (auto lobj = S->VortexList.begin(); lobj < S->VortexList.end(); )
-	{
-		TAtt* bad_segment = NULL;
-		TBody* bad_body = NULL;
-		for (auto& lbody: S->BodyList)
-		{
-			bad_segment = lbody->isPointInvalid(lobj->r);
-			if (bad_segment) { bad_body = lbody.get(); break; }
-		}
-		if (!bad_body) { lobj++; continue; }
+        // check if particle is in heat layer
+        bad_segment = NULL;
+        bad_body = NULL;
+        for (auto& lbody: S->BodyList)
+        {
+            bad_segment = lbody->isPointInHeatLayer(lobj->r);
+            if (bad_segment) { bad_body = lbody.get(); break; }
+        }
 
-		bad_body->force_dead.r += rotl(lobj->r) * lobj->g;
-		bad_body->force_dead.o += (lobj->r - bad_body->get_axis()).abs2() * lobj->g;
-		bad_segment->gsum -= lobj->g;
-		bad_body->g_dead += lobj->g;
-		CleanedV_++;
-		S->VortexList.erase(lobj);
-	}
+        if ( !bad_body || bad_body->heat_condition != hc_t::const_t) { lobj++; continue; }
 
-	for (auto lobj = S->HeatList.begin(); lobj < S->HeatList.end(); )
-	{
-		TAtt* bad_segment = NULL;
-		TBody* bad_body = NULL;
-		// check if particle is in body
-		for (auto& lbody: S->BodyList)
-		{
-			bad_segment = lbody->isPointInvalid(lobj->r);
-			if (bad_segment) { bad_body = lbody.get(); break; }
-		}
+        if (bad_segment->heat_layer_obj_no >= 0)
+        {
+            bad_segment->hsum -= lobj->g;
+            S->HeatList.erase(lobj);
+            continue;
+        } else
+        {
+            bad_segment->heat_layer_obj_no = lobj - S->HeatList.begin();
+            lobj++;
+            continue;
+        }
+    }
 
-		if (bad_body)
-		{
-			bad_segment->hsum -= lobj->g;
-			S->HeatList.erase(lobj);
-			continue;
-		}
+    for (auto lobj = S->StreakList.begin(); lobj < S->StreakList.end(); )
+    {
+        if ( S->PointIsInBody(lobj->r) ) S->StreakList.erase(lobj);
+        else lobj++;
+    }
 
-		// check if particle is in heat layer
-		bad_segment = NULL;
-		bad_body = NULL;
-		for (auto& lbody: S->BodyList)
-		{
-			bad_segment = lbody->isPointInHeatLayer(lobj->r);
-			if (bad_segment) { bad_body = lbody.get(); break; }
-		}
+    if (zero_speed)
+    {
+        for (auto& lobj: S->VortexList) lobj.v = TVec(0., 0.);
+        for (auto& lobj: S->HeatList)   lobj.v = TVec(0., 0.);
+        for (auto& lobj: S->StreakList) lobj.v = TVec(0., 0.);
+    }
 
-		if ( !bad_body || bad_body->heat_condition != hc_t::const_t) { lobj++; continue; }
-
-		if (bad_segment->heat_layer_obj_no >= 0)
-		{
-			bad_segment->hsum -= lobj->g;
-			S->HeatList.erase(lobj);
-			continue;
-		} else
-		{
-			bad_segment->heat_layer_obj_no = lobj - S->HeatList.begin();
-			lobj++;
-			continue;
-		}
-	}
-
-	for (auto lobj = S->StreakList.begin(); lobj < S->StreakList.end(); )
-	{
-		if ( S->PointIsInBody(lobj->r) ) S->StreakList.erase(lobj);
-		else lobj++;
-	}
-
-	if (zero_speed)
-	{
-		for (auto& lobj: S->VortexList) lobj.v = TVec(0., 0.);
-		for (auto& lobj: S->HeatList)   lobj.v = TVec(0., 0.);
-		for (auto& lobj: S->StreakList) lobj.v = TVec(0., 0.);
-	}
-
-	for (auto& lbody: S->BodyList)
-	{
-		lbody->force_dead.r /= dt;
-		lbody->force_dead.o /= 2.*dt;
-	}
+    for (auto& lbody: S->BodyList)
+    {
+        lbody->force_dead.r /= dt;
+        lbody->force_dead.o /= 2.*dt;
+    }
 }
 
 void flowmove::VortexShed()
 {
-	for (auto& lbody: S->BodyList)
-	{
-		lbody->force_born = TVec3D(0., 0., 0.);
+    for (auto& lbody: S->BodyList)
+    {
+        lbody->force_born = TVec3D(0., 0., 0.);
 
-		for (auto& latt: lbody->alist)
-		{
-			if (fabs(latt.g) < RemoveEps)
-			{
-				CleanedV_++;
-				lbody->g_dead+= latt.g;
-			}
-			else if ( !latt.slip )
-			{
-				S->VortexList.push_back(TObj(latt.corner + rotl(latt.dl)*1e-4, latt.g));
-				lbody->force_born.r += rotl(latt.corner) * latt.g;
-				lbody->force_born.o += (latt.corner - lbody->get_axis()).abs2() * latt.g;
-				latt.gsum+= latt.g;
-			}
-		}
+        for (auto& latt: lbody->alist)
+        {
+            if (fabs(latt.g) < RemoveEps)
+            {
+                CleanedV_++;
+                lbody->g_dead+= latt.g;
+            }
+            else if ( !latt.slip )
+            {
+                S->VortexList.push_back(TObj(latt.corner + rotl(latt.dl)*1e-4, latt.g));
+                lbody->force_born.r += rotl(latt.corner) * latt.g;
+                lbody->force_born.o += (latt.corner - lbody->get_axis()).abs2() * latt.g;
+                latt.gsum+= latt.g;
+            }
+        }
 
-		lbody->force_born.r /= dt;
-		lbody->force_born.o /= 2.*dt;
-	}
+        lbody->force_born.r /= dt;
+        lbody->force_born.o /= 2.*dt;
+    }
 }
 
 void flowmove::HeatShed()
 {
-	for (auto& lbody: S->BodyList)
-	{
-		if (lbody->heat_condition == hc_t::isolate)
-		for (auto& latt: lbody->alist)
-		{
-			if (!latt.hsum) continue;
-			S->HeatList.push_back(TObj(latt.r+rotl(latt.dl)*0.5, -latt.hsum));
-			latt.hsum = 0;
-		}
+    for (auto& lbody: S->BodyList)
+    {
+        if (lbody->heat_condition == hc_t::isolate)
+            for (auto& latt: lbody->alist)
+            {
+                if (!latt.hsum) continue;
+                S->HeatList.push_back(TObj(latt.r+rotl(latt.dl)*0.5, -latt.hsum));
+                latt.hsum = 0;
+            }
 
-		else if (lbody->heat_condition == hc_t::const_t)
-		for (auto& latt: lbody->alist)
-		{
-			double tmp_g = latt.dl.abs2() * latt.heat_const;
-			TObj *tmp_obj = (latt.heat_layer_obj_no>=0)? S->HeatList.data()+latt.heat_layer_obj_no : NULL;
-			if (tmp_obj)
-			{
-				latt.hsum += tmp_g - tmp_obj->g;
-				tmp_obj->g = tmp_g;
-			} else
-			{
-				latt.hsum += tmp_g;
-				S->HeatList.push_back(TObj(latt.r+rotl(latt.dl)*0.5, tmp_g));
-			}
-		}
+        else if (lbody->heat_condition == hc_t::const_t)
+            for (auto& latt: lbody->alist)
+            {
+                double tmp_g = latt.dl.abs2() * latt.heat_const;
+                TObj *tmp_obj = (latt.heat_layer_obj_no>=0)? S->HeatList.data()+latt.heat_layer_obj_no : NULL;
+                if (tmp_obj)
+                {
+                    latt.hsum += tmp_g - tmp_obj->g;
+                    tmp_obj->g = tmp_g;
+                } else
+                {
+                    latt.hsum += tmp_g;
+                    S->HeatList.push_back(TObj(latt.r+rotl(latt.dl)*0.5, tmp_g));
+                }
+            }
 
-		else if (lbody->heat_condition == hc_t::const_w)
-		for (auto& latt: lbody->alist)
-		{
-			double tmp_g = latt.dl.abs2() * latt.heat_const;
-			latt.hsum += tmp_g;
-			S->HeatList.push_back(TObj(latt.r+rotl(latt.dl)*0.5, tmp_g));
-		}
-	}
+        else if (lbody->heat_condition == hc_t::const_w)
+            for (auto& latt: lbody->alist)
+            {
+                double tmp_g = latt.dl.abs2() * latt.heat_const;
+                latt.hsum += tmp_g;
+                S->HeatList.push_back(TObj(latt.r+rotl(latt.dl)*0.5, tmp_g));
+            }
+    }
 }
 
 void flowmove::CropHeat(double scale)
 {
-	for (auto lobj = S->HeatList.begin(); lobj < S->HeatList.end(); )
-	{
-		if (lobj->r.x > scale) S->HeatList.erase(lobj);
-		else lobj++;
-	}
+    for (auto lobj = S->HeatList.begin(); lobj < S->HeatList.end(); )
+    {
+        if (lobj->r.x > scale) S->HeatList.erase(lobj);
+        else lobj++;
+    }
 }
 
 void flowmove::StreakShed()
 {
-	if (!S->Time.divisibleBy(S->streak_dt)) return;
-	for (auto& lobj: S->StreakSourceList)
-	{
-		S->StreakList.push_back(lobj);
-	}
+    if (!S->Time.divisibleBy(S->streak_dt)) return;
+    for (auto& lobj: S->StreakSourceList)
+    {
+        S->StreakList.push_back(lobj);
+    }
 }
 
