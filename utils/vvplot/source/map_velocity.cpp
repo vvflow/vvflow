61e1194c0a35c98591651170fd435754fa33a290
Yaroslav Dynnikov <rosik@rlap>
2015-07-24 13:11:03 +0300

    [fix] forgotten file
    Former-commit-id: 1b728a5194d5d49dc4e32eb83403ce41a4193ea5

---
+++ utils/vvplot/source/map_velocity.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <math.h>
+#include <time.h>
+
+#include "libvvplot_api.h"
+#include "convectivefast.h"
+#include "core.h"
+#include "hdf5.h"
+
+static double Rd2;
+static TVec RefFrame_Speed;
+inline static double atan(TVec p) {return atan(p.y/p.x);}
+
+extern "C" {
+int velocity_print(hid_t fid, TVec* points, int count)
+{
+	Space *S = new Space();
+	S->Load(fid);
+
+	double dl = S->AverageSegmentLength(); Rd2 = dl*dl/25;
+	S->Tree = new TSortedTree(S, 8, dl*20, 0.3);
+	convectivefast conv(S);
+	S->Tree->build();
+
+	for (int i=0; i<count; i++)
+	{
+        TVec p = points[i];
+        TVec v = conv.SpeedSumFast(p);
+        printf("%.6le\t %.6le\n", v.x, v.y);
+	}
+
+	return 0;
+}}
