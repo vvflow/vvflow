a9b14e8a511d471e72b65411246a46777a6ec412
Yaroslav Dynnikov <rosik@rlap>
2015-12-21 01:42:14 +0300

    [add] vvhd.mk
    Former-commit-id: 551c7d48fb859d567f543404d690c23107e0d487

---
+++ vvhd.mk
@@ -0,0 +1,16 @@
+PREFIX = $(HOME)/.local
+LDLIBS := -lhdf5 -lmatheval
+
+ifeq ($(CXX),icpc)
+	CXXFLAGS+= -g -debug all -O3 -Wall -openmp -mkl
+	LDFLAGS+= -mkl=parallel
+	AR = xiar
+else
+	CXXFLAGS+= -O3 -g -Wall -fopenmp
+	LDLIBS += -llapack
+endif
+
+$(PREFIX)/%:
+	mkdir $@ -p
+
+.PHONY: all clean install uninstall
