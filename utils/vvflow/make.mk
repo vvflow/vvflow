bd49c8a83af42ab377397bfd1746998a6c82db94
Yaroslav Dynnikov <rosik@rlap>
2016-01-06 03:29:53 +0300

    [fix] makefiles again
    Former-commit-id: e3aa8f0fb907c59a384432b6952d7bc4095b0400

--- utils/vvflow/make.mk
+++ utils/vvflow/make.mk
@@ -2,8 +2,9 @@ TARGETS_ALL       += bin/vvflow
 TARGETS_INSTALL   += vvflow_install
 TARGETS_UNINSTALL += vvflow_uninstall
 
-bin/vvflow: vvflow.cpp  bin/libvvhd.so
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -lvvhd $< -o ./$@
+bin/vvflow: bin/libvvhd.so
+bin/vvflow: vvflow.cpp
+	$(CXX) $(CXXFLAGS) $^ $(LDFLAGS) -lvvhd -o ./$@
 
 vvflow_install: | $(PREFIX)/bin
 	cp ./bin/vvflow -t $(PREFIX)/bin/
--- utils/vvxtract/make.mk
+++ utils/vvxtract/make.mk
@@ -2,8 +2,9 @@ TARGETS_ALL       += bin/vvxtract
 TARGETS_INSTALL   += vvxtract_install
 TARGETS_UNINSTALL += vvxtract_uninstall
 
-bin/vvxtract: vvxtract.cpp bin/libvvhd.so
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -lvvhd $< -o ./$@
+bin/vvxtract: bin/libvvhd.so
+bin/vvxtract: vvxtract.cpp
+	$(CXX) $(CXXFLAGS) $^ $(LDFLAGS) -lvvhd -o ./$@
 
 vvxtract_install: h5xtract | $(PREFIX)/bin
 	cp ./bin/vvxtract -t $(PREFIX)/bin/
