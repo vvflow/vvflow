912710e594206bafd29c4c98ad0a8832b4248c82
Yaroslav Dynnikov <rosik@rlap>
2016-02-26 01:21:31 +0300

    [add] vvgen.gis
    Former-commit-id: 7bd60735f1cf795fc2c73d70436e13f0a6a562d7

--- utils/scripts/make.mk
+++ utils/scripts/make.mk
@@ -2,7 +2,7 @@ TARGETS_INSTALL   += vvscripts_install
 TARGETS_UNINSTALL += vvscripts_uninstall
 VVSCRIPTS = gpquick vvencode
 VVSCRIPTS += vvawk.avg vvawk.mavg vvawk.sd vvawk.zeros
-VVSCRIPTS += vvgen.plate vvgen.cylinder vvgen.semicyl
+VVSCRIPTS += vvgen.plate vvgen.cylinder vvgen.semicyl vvgen.gis
 
 vvscripts_install: $(VVSCRIPTS) | $(PREFIX)/bin
 	$(foreach f,$^,\
