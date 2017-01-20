d6150b4f8b13da0cc424f8870187de3950eaa8e0
Yaroslav Dynnikov <rosik@rlap>
2017-01-20 03:06:38 +0300

    [enh] libvvplot: calc map_vorticity from command line
    Former-commit-id: 1f266fe182bbe9f449673c22ed40f2758334f45a

--- utils/vvplot/source/libvvplot_main.cpp
+++ utils/vvplot/source/libvvplot_main.cpp
@@ -19,12 +19,17 @@ void print_help()
 	fprintf(stderr, "Options:\n");
 	fprintf(stderr, " -h : show this message\n");
 	fprintf(stderr, " -v : show version info\n");
-	fprintf(stderr, " -p : print dataset from hdf file in plain text\n");
-	fprintf(stderr, " -M : extract a binary dataset from hdf file (x, y, value)\n");
-	fprintf(stderr, " -m : extract a binary dataset from hdf file (binary matrix)\n");
-	fprintf(stderr, " -V : calculate velocity args of format px,py\n");
-	fprintf(stderr, " -L : extract a list of domains from hdf file\n");
-	fprintf(stderr, " -I : plot isolines on a dataset with constants in args\n");
+
+	fprintf(stderr, " -p : extract DATASET as plain text\n");
+	fprintf(stderr, " -L : extract DATASET as binary matrix\n");
+	fprintf(stderr, " -m : extract DATASET as binary matrix with prepended XMIN XMAX YMIN YMAX SPACING (double)\n");
+	fprintf(stderr, " -M : extract DATASET as binary matrix in form (x, y, value)\n");
+
+	fprintf(stderr, " -V : calculate velocity; DATASET ignored; ARGS: px1,py1 [px2,py2 ...]\n");
+	fprintf(stderr, " -I : plot isolines on a DATASET; ARGS: c1 [c2 ...]\n");
+
+	fprintf(stderr, " --vorticity : plot map_vorticity(); DATASET ignored; ARGS: XMIN XMAX YMIN YMAX SPACING\n");
+
 	fflush(stderr);
 }
 
@@ -37,7 +42,9 @@ int main(int argc, char **argv)
 	else if (argc < 4) { print_help(); exit(1); }
 	else if (!strcmp(argv[1], "-I") || !strcmp(argv[1], "-p") ||
 	         !strcmp(argv[1], "-M") || !strcmp(argv[1], "-L") ||
-             !strcmp(argv[1], "-m") || !strcmp(argv[1], "-V")) {;}
+             !strcmp(argv[1], "-m") || !strcmp(argv[1], "-V") ||
+             !strcmp(argv[1], "--vorticity")
+             ) {;}
 	else {fprintf(stderr, "Bad option '%s'. See '-h' for help.\n", argv[1]); exit(-1); }
 
 	H5Eset_auto2(H5E_DEFAULT, NULL, NULL);
@@ -46,7 +53,7 @@ int main(int argc, char **argv)
 	{
 		H5Epop(H5E_DEFAULT, H5Eget_num(H5E_DEFAULT)-1);
 		H5Eprint2(H5E_DEFAULT, stderr);
-		fprintf(stderr, "error: argument file: can't open file '%s'\n", argv[2]);
+		fprintf(stderr, "error: argument FILE: can't open file '%s'\n", argv[2]);
 		return 2;
 	}
 
@@ -89,12 +96,42 @@ int main(int argc, char **argv)
             int len;
             if (sscanf(argv[i], "%f%n", &vals[i-4], &len)!=1 || argv[i][len])
             {
-                fprintf(stderr, "error: argument should be float");
+                fprintf(stderr, "error: argument ARGS: must be float");
                 exit(-1);
             }
         }
 		map_isoline(fid, argv[3], vals, argc-4);
         free(vals);
+	}
+	else if (!strcmp(argv[1], "--vorticity"))
+	{
+		if (argc != 9)
+		{
+			fprintf(stderr, "error: argument ARGS: must be XMIN XMAX YMIN YMAX SPACING\n");
+            exit(-1);
+		}
+
+		H5Fclose(fid);
+		fid = H5Fopen(argv[2], H5F_ACC_RDWR, H5P_DEFAULT);
+		if (fid < 0)
+		{
+			H5Epop(H5E_DEFAULT, H5Eget_num(H5E_DEFAULT)-1);
+			H5Eprint2(H5E_DEFAULT, stderr);
+			fprintf(stderr, "error: argument FILE: can't reopen file '%s' for writing\n", argv[2]);
+			return 2;
+		}
+
+		double args[5];
+		for (int i=4; i<9; i++)
+        {
+            int len;
+            if (sscanf(argv[i], "%lf%n", &args[i-4], &len)!=1 || argv[i][len])
+            {
+                fprintf(stderr, "error: argument ARGS: must be float");
+                exit(-1);
+            }
+        }
+        map_vorticity(fid, args[0], args[1], args[2], args[3], args[4]);
 	}
 	H5Fclose(fid);
 
