71e85ecf509dbac28141bef97020c1522a43b440
Yaroslav Dynnikov <rosik@rlap>
2017-09-25 02:26:07 +0300

    [enh] TEval tests
    Former-commit-id: 606abcdc48ec41c8d1227ba7ba2ffcbeb1feb740

--- libvvhd/test/test_TEval.hpp
+++ libvvhd/test/test_TEval.hpp
@@ -10,6 +10,7 @@ class TEvalTest : public ::testing::Test {};
 TEST_F(TEvalTest, ConstructDefault)
 {
     TEval eval;
+    EXPECT_EQ("", std::string(eval));
     EXPECT_EQ(0, eval.eval(-1));
     EXPECT_EQ(0, eval.eval(0));
     EXPECT_EQ(0, eval.eval(1));
@@ -22,8 +23,10 @@ TEST_F(TEvalTest, ConstructWithString)
     EXPECT_THROW(TEval eval(")");, std::invalid_argument);
     // invalid expression
     EXPECT_THROW(TEval eval("a+1");, std::invalid_argument);
+    EXPECT_THROW(TEval eval("t(1)");, std::invalid_argument);
+    EXPECT_THROW(TEval eval("x(1)");, std::invalid_argument);
     
-    TEval eval("");
+    TEval eval("t-t");
     eval = "t+1";
     // invalid expression should not spoil evaluator
     EXPECT_THROW(eval = "b+1";, std::invalid_argument);
@@ -35,8 +38,16 @@ TEST_F(TEvalTest, ConstructWithString)
     EXPECT_EQ(3, eval.eval(1));
     
     // copy and move compile
-    TEval e2(eval);
-    TEval e3; e3 = eval;
-    TEval e4(std::move(eval));
-    TEval e5; e5 = std::move(eval);
+    const TEval e1("sin(t)");
+    TEval e2(e1);
+    EXPECT_EQ(e2.eval(0.1), std::sin(0.1));
+
+    TEval e3; e3 = e1;
+    EXPECT_EQ(e3.eval(0.2), std::sin(0.2));
+    
+    TEval e4(std::move(e1));
+    EXPECT_EQ(e4.eval(0.3), std::sin(0.3));
+    
+    TEval e5; e5 = std::move(e1);
+    EXPECT_EQ(e5.eval(0.4), std::sin(0.4));
 }
