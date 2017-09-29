f40c11d8cccda647dafd8094c08cef6c3e1032db
Yaroslav Dynnikov <rosik@rlap>
2017-09-30 02:45:54 +0300

    [dev] gen_ellipse works
    Former-commit-id: 14b42990fa7d430d6a4d11e5a47419f3b63320aa

--- utils/vvcompose/gen_ellipse.cpp
+++ utils/vvcompose/gen_ellipse.cpp
@@ -33,47 +33,34 @@ int luavvd_gen_ellipse(lua_State *L) {
         luaL_argerror(L, 1, lua_tostring(L, -1));
     }
 
-    std::shared_ptr<TBody> body = std::make_shared<TBody>();
     if (!dl) {
         // Ramanujan (II)
         double h = sqr((Rx-Ry)/(Rx+Ry));
         double slen = M_PI*(Rx+Ry)*(1. + 3*h/(10+sqrt(4-3*h)));
         dl = slen/N;
     }
-    luaL_argcheck(L, dl<Ry/2, 1, "discretization too low");
+    luaL_argcheck(L, dl<Ry/1.9, 1, "discretization too low");
 
-    std::vector<double> q1;
+    std::vector<double> semiell;
     for (double a=asin(dl/(2*Ry)); a<M_PI-asin(dl/(2*Ry)); ) {
-        printf("%zd %lf\n", q1.size(), a);
-        q1.push_back(a);
+        semiell.push_back(a);
         double da1 = dl / sqrt( sqr(Rx*sin(a)) + sqr(Ry*cos(a)) );
         double da2 = dl / sqrt( sqr(Rx*sin(a+da1)) + sqr(Ry*cos(a+da1)) );
         a += (da1+da2)/2;
     }
-    fprintf(stderr, "q1: %zd\n", q1.size());
 
-    // dl*=1.1;
-
-    std::vector<double> q2;
-    for (double a=M_PI-asin(dl/(2*Ry)); a>asin(dl/(2*Ry)); ) {
-        q2.push_back(a);
-        printf("%zd %lf\n", q1.size()-q2.size(), a);
-        double da1 = dl / sqrt( sqr(Rx*sin(a)) + sqr(Ry*cos(a)) );
-        double da2 = dl / sqrt( sqr(Rx*sin(a-da1)) + sqr(Ry*cos(a-da1)) );
-        a -= (da1+da2)/2;
+    std::shared_ptr<TBody> body = std::make_shared<TBody>();
+    N = semiell.size()-1;
+    // emplace bottom half
+    for (double i=0; i<=N; i++) {
+        double a = semiell[i]*(1.-i/N) + (M_PI-semiell[N-i])*(i/N);
+        body->alist.emplace_back(Rx*cos(a), -Ry*sin(a));
     }
-    fprintf(stderr, "q2: %zd\n", q2.size());
-
-
-
-        // ai, xp, yp = (0, None, None)
-        // for n in range(N*5000):
-        //     xi = Rx*cos(2*pi*n/N/5000)
-        //     yi = -Ry*sin(2*pi*n/N/5000)
-        //     if xp and yp and sqrt((xi-xp)**2+(yi-yp)**2) < dl:
-        //         continue
-        //     print("{:+0.6e}\t {:+0.6e}".format(xi, yi))
-        //     xp, yp = (xi, yi)
+    // emplace top half
+    for (double i=0; i<=N; i++) {
+        double a = semiell[i]*(1.-i/N) + (M_PI-semiell[N-i])*(i/N);
+        body->alist.emplace_back(-Rx*cos(a), Ry*sin(a));
+    }
 
     body->doUpdateSegments();
     body->doFillProperties();
