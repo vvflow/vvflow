16667fd7d69313bc336b961247ad12442d1d0290
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2024-08-23 01:22:36 +0300

    Add more pytests for vvflow

--- pytest/vvflow/test_vvflow.py
+++ pytest/vvflow/test_vvflow.py
@@ -15,3 +15,26 @@ def test_args(env: Env):
         "--progress",
         "boo"])
     assert ret.stdout == b"bar\nbaz\nboo\n"
+
+
+def test_run1(env: Env):
+    s = """
+        print("hello vvflow")
+    """
+    with open(env.tmp("run1.lua"), "w") as f:
+        f.write(s)
+
+    ret = env.run(["vvflow", env.tmp("run1.lua")])
+    assert ret.stdout == b"hello vvflow\n"
+    assert ret.stderr == b""
+
+
+def test_run2(env: Env):
+    s = """
+        error("artificial error")
+    """
+    with open(env.tmp("run2.lua"), "w") as f:
+        f.write(s)
+
+    ret = env.run(["vvflow", env.tmp("run2.lua")])
+    assert ret.stderr.decode("utf-8") == tempdir+"/run2.lua:2: artificial error\n"
