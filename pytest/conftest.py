68dd96d96b13ae566587415ea12e80fdd2d936eb
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2024-08-23 00:42:51 +0300

    pytests: fix linter errors

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -8,18 +8,19 @@ import tempfile
 import subprocess
 logging.basicConfig(level=logging.INFO, format=' %(levelname)7s %(name)s > %(message)s')
 
+
 class Env:
     def __init__(self, cwd, tempdir):
         self.cwd = cwd
         self.tempdir = tempdir
 
         self.env = os.environ.copy()
-        path = [
-            'utils/vvcompose',
-            'utils/vvxtract',
-            'utils/vvplot',
-            self.env['PATH']
-        ]
+        # path = [
+        #     'utils/vvcompose',
+        #     'utils/vvxtract',
+        #     'utils/vvplot',
+        #     self.env['PATH']
+        # ]
         # self.env['PATH'] = ':'.join(path)
         logging.warn('export PATH="%s"' % self.env['PATH'])
 
@@ -30,12 +31,13 @@ class Env:
             del kwargs["timeout"]
 
         logging.info("Running: '{}'".format(cmd))
-        proc = subprocess.Popen(cmd,
+        proc = subprocess.Popen(
+            cmd,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             env=self.env,
             **kwargs
-            )
+        )
 
         try:
             stdout, stderr = proc.communicate(timeout=timeout)
@@ -46,20 +48,23 @@ class Env:
         if stdout:
             logger = logging.getLogger('stdout:')
             try:
-                for l in iter(stdout.splitlines()):
-                    logger.info(l.decode('utf-8'))
+                for line in iter(stdout.splitlines()):
+                    logger.info(line.decode('utf-8'))
             except UnicodeError:
                 logger.info("not UTF-8 ({} B)".format(len(stdout)))
             del logger
 
         if stderr:
             logger = logging.getLogger('stderr:')
-            for l in iter(stderr.splitlines()):
-                logger.error(l.decode('utf-8'))
+            for line in iter(stderr.splitlines()):
+                logger.error(line.decode('utf-8'))
             del logger
 
         if proc.returncode != 0:
-            raise subprocess.CalledProcessError(returncode=proc.returncode, cmd=cmd)
+            raise subprocess.CalledProcessError(
+                returncode=proc.returncode,
+                cmd=cmd
+            )
 
         class ProcOutput():
             pass
@@ -95,7 +100,7 @@ class Env:
         output = self.run(
             ['vvplot', ifile, ofile] + args,
             cwd=self.tempdir,
-            timeout = 30
+            timeout=30
         )
         output.ifile = ifile
         output.ofile = ofile
@@ -120,6 +125,7 @@ def tempdir(request):
             os.makedirs(tempdir)
         return tempdir
 
+
 @pytest.fixture(scope='module')
 def env(request, tempdir):
     env = Env(
@@ -129,5 +135,3 @@ def env(request, tempdir):
     return env
     # dir = os.path.join(request.fspath.dirname, 'output')
     # return str(dir)
-
-
