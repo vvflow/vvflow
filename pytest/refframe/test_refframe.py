4fef496b8ab7f322713d9a9424a19ba56bbecd79
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2024-11-20 22:37:31 +0300

    pytest: fix flake8 warnings

--- pytest/refframe/test_refframe.py
+++ pytest/refframe/test_refframe.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import os
 import numpy
 import struct
 import logging
-import subprocess
 
 tempdir = "/tmp/test_refframe"
 
--- pytest/vvplot/test_basic.py
+++ pytest/vvplot/test_basic.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os
 import pytest
 
 tempdir = "/tmp/test_vvplot"
