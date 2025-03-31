f8064f4e101d4dec468b4c085d5fdb4cbd3d6f41
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2025-04-01 02:37:19 +0300

    test: add test_pidfile

--- utils/vvflow/utils.cpp
+++ utils/vvflow/utils.cpp
@@ -1,4 +1,4 @@
-#include "communicate.h"
+#include "utils.h"
 
 #include <cerrno>
 #include <cstdio>
@@ -21,7 +21,7 @@ PidFile::PidFile(const std::string& sim_caption):
 {
     fd = open(pidfile_path.c_str(), O_RDWR | O_CREAT, 0644);
     if (fd < 0) {
-        throw std::runtime_error("error opening pidfile: " + pidfile_path + ": " + errno_str());
+        throw std::runtime_error("Error opening pidfile: " + pidfile_path + ": " + errno_str());
     }
 
     if (flock(fd, LOCK_EX | LOCK_NB) < 0) {
@@ -30,14 +30,20 @@ PidFile::PidFile(const std::string& sim_caption):
         size_t ret = read(fd, buf, sizeof(buf)-1);
         close(fd);
         if (ret <= 0) {
-            throw std::runtime_error("simulation already running: " + sim_caption);
+            throw std::runtime_error("Simulation already running: " + sim_caption);
         } else {
             buf[ret] = '\0';
-            throw std::runtime_error("simulation already running: " + sim_caption + ", pid: " + buf);
+            if (char* nl = strchr(buf, '\n')) {*nl = '\0';}
+            throw std::runtime_error("Simulation already running: " + sim_caption + ", pid: " + buf);
         }
     }
 
     (void)!ftruncate(fd, 0);
+}
+
+PidFile::~PidFile() {
+    flock(fd, LOCK_UN);
+    std::remove(pidfile_path.c_str());
 }
 
 void PidFile::write() {
@@ -53,13 +59,13 @@ ListenSock::ListenSock(const std::string& sim_caption):
 {
     fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (fd < 0) {
-        throw std::runtime_error("socket error: " + errno_str());
+        throw std::runtime_error("Socket error: " + errno_str());
     }
 
     struct sockaddr_un addr;
     memset(&addr, 0, sizeof(struct sockaddr_un));
     if (sock_path.length() > sizeof(addr.sun_path) - 1) {
-        throw std::runtime_error("socket error: " + sock_path + ": filename too long");
+        throw std::runtime_error("Socket error: " + sock_path + ": filename too long");
     }
     strncpy(addr.sun_path, sock_path.c_str(), sizeof(addr.sun_path) - 1);
     unlink(addr.sun_path);
@@ -68,15 +74,15 @@ ListenSock::ListenSock(const std::string& sim_caption):
     int ret = bind(fd, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
     if (ret < 0) {
         if (errno == EADDRINUSE) {
-            throw std::runtime_error("simulation already running: " + sim_caption);
+            throw std::runtime_error("Simulation already running: " + sim_caption);
         } else {
-            throw std::runtime_error("socket bind error: " + sock_path + ": " + errno_str());
+            throw std::runtime_error("Socket bind error: " + sock_path + ": " + errno_str());
         }
     }
 
     ret = listen(fd, 20);
     if (ret < 0) {
-        throw std::runtime_error("socket listen error: " + sock_path + ": " + errno_str());
+        throw std::runtime_error("Socket listen error: " + sock_path + ": " + errno_str());
     }
 }
 
